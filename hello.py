5f0880f38366ae65e576fc82197fdbe305fa10ff
25442570 <1292555877@qq.com>
2026-02-04 11:55:22 +0800

    Lab3

--- hello.py
+++ hello.py
@@ -1,5 +1,15 @@
+
+# Write a function that prints all factors of the given parameter x
+def print_factor(x):
+        print(f"Factors of {x}:")
+        for i in range(1, x):
+            if x % i == 0:
+                print(i, end=' ')
+
+
 def main():
-    print("Hello World")
+    x = 52633
+    print_factor(x)
 
 if __name__ == '__main__':
     main()
